375dd93af9f754e6b78fb364ebc8be497de173bb
jmc <jmc@teammccaffrey.com>
2024-12-12 22:17:55 -0800

    Day 09 solved

--- Days/Day09/Solution.fs
+++ Days/Day09/Solution.fs
@@ -5,11 +5,12 @@ open FsUnit
 open AoC2024.Util
 
 let exampleInput = "2333133121414131402"
+   
     
 type DiskMapItem =
     | File of (int * int)
     | Free of int
-    
+   
 let parseDiskMap (str: string) : DiskMapItem list =
     str
     |> ParseInput.intString
@@ -18,6 +19,10 @@ let parseDiskMap (str: string) : DiskMapItem list =
         | i, n when (i % 2 = 0) -> File (i / 2, n)
         | _, n -> Free n)
     |> Seq.toList
+
+let isFile = (function
+    | File _ -> true
+    | _ -> false)
 
 module PartOne =
     
@@ -56,9 +61,7 @@ module PartOne =
         // A reversed list of only the file blocks. This makes it easier to pull file blocks to fill
         let reversedFiles =
             diskMap
-            |> List.filter (function
-                | File _ -> true
-                | _ -> false)
+            |> List.filter isFile 
             |> List.rev
         
         inner diskMap reversedFiles
@@ -84,13 +87,122 @@ module PartOne =
 
 module PartTwo =
  
-    // Given a disk map, emit a stream of file ids, counts, and offsets representing defragmented files 
-    let defragFiles (diskMap: DiskMapItem list) : (int * int * int) seq =
-        0
-     
+    // Given a disk map, emit a stream of file ids, and offsets representing defragmented files 
+    // let defragFiles (diskMap: DiskMapItem list) : (int * int) seq =
+    //   
+    //     let defragOneFile (diskMap: DiskMapItem list) (fileIdx: int, file: DiskMapItem) =
+    //         let (File (_, fileSize)) = file
+    //         
+    //         let res =
+    //             diskMap
+    //             |> Seq.take fileIdx
+    //             |> Seq.indexed
+    //             |> Seq.tryFind(function
+    //                 | _, Free n -> n >= fileSize
+    //                 | _ -> false)
+    //    
+    //         match res with
+    //         | None -> diskMap
+    //         
+    //         | Some (i, Free n) when n = fileSize ->
+    //             diskMap
+    //             |> List.updateAt fileIdx (Free n)
+    //             |> List.updateAt i file
+    //             
+    //         | Some (i, Free n) ->
+    //             diskMap
+    //             |> List.updateAt fileIdx (Free fileSize)
+    //             |> List.updateAt i (Free (n - fileSize))
+    //             |> List.insertAt i file
+    //         
+    //         
+    //     let expandBlock (offset: int) (block: DiskMapItem) : ((int * int) seq * int) =
+    //         let idx, n =
+    //             match block with
+    //             | File (idx, n) -> idx, n
+    //             | Free n -> 0, n
+    //         
+    //         let blocks =
+    //             [0..(n-1)]
+    //             |> Seq.map (fun i -> (idx, offset + i))
+    //         
+    //         (blocks, offset + n)
+    //     
+    //     diskMap
+    //     |> Seq.indexed
+    //     |> Seq.filter (snd >> isFile)
+    //     |> Seq.rev
+    //     |> Seq.fold defragOneFile diskMap
+    //     |> Seq.mapFold expandBlock 0
+    //     |> fst
+    //     |> Seq.concat
+    //
+    
+    let defragFiles (diskMap: DiskMapItem list) : (int * int) seq =
+      
+        let rec inner (diskMap: DiskMapItem list) =
+            seq {
+                match diskMap with
+                | [] -> ()
+                | Free n :: rest ->
+                    yield (Free n)
+                    yield! inner rest
+                
+                | File (idx, n) :: rest ->
+                    
+                    let file = File (idx, n)
+                    let freeSpace =
+                        rest
+                        |> Seq.indexed
+                        |> Seq.tryFindBack (
+                            snd
+                            >> (function
+                                | Free space -> space >= n
+                                | _ -> false))
+                    
+                    match freeSpace with
+                    | None ->
+                        yield file
+                        yield! inner rest
+                        
+                    | Some (i, (Free space)) when space = n ->
+                        yield (Free space)
+                        yield! inner (List.updateAt i file rest)
+                        
+                    | Some (i, (Free space)) when space > n ->
+                        yield (Free n)
+                        
+                        let edit =
+                            rest
+                            |> List.updateAt i (Free (space - n))
+                            |> List.insertAt (i + 1) file
+                            
+                        yield! inner edit
+            }
+        
+        let expandBlock (offset: int) (block: DiskMapItem) : ((int * int) seq * int) =
+            let idx, n =
+                match block with
+                | File (idx, n) -> idx, n
+                | Free n -> 0, n
+            
+            let blocks =
+                [0..(n-1)]
+                |> Seq.map (fun i -> (idx, offset + i))
+        
+            (blocks, offset + n)
+            
+        diskMap
+        |> List.rev
+        |> inner
+        |> Seq.rev
+        |> Seq.mapFold expandBlock 0
+        |> fst
+        |> Seq.concat
+            
     let solve =
         defragFiles
-        >> Seq.map (fun (idx, n, offset) -> int64 (idx * n * offset))
+        >> Seq.map (fun (idx, n) -> int64 (idx * n))
         >> Seq.sum
         
     let parseAndSolve = parseDiskMap >> solve
@@ -100,3 +212,9 @@ module PartTwo =
         exampleInput
         |> parseAndSolve
         |> should equal 2858
+
+    [<Test>]
+    let ``problem input``() =
+        readTextFromFile @"Days/Day09/input.txt"
+        |> parseAndSolve
+        |> should equal 6511178035564L
