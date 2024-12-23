8859174e22d1cd1eb88b3b1ae2be758775714b44
jmc <jmc@teammccaffrey.com>
2024-12-22 22:48:14 -0800

    Day 17 solved

--- Days/Day17/Solution.fs
+++ Days/Day17/Solution.fs
@@ -20,7 +20,7 @@ let regToInt = function
     | C -> 2
    
 type ProgramState = {
-    regs: int array
+    regs: int64 array
     ops: int array
     ip: int
     out: int list
@@ -33,7 +33,7 @@ let parseProgram (str: string) : ProgramState =
     
     let parseRegister (line: string) =
         let m = Regex.Match(line, @"Register ([A-Z]): (\d+)")
-        (m.Groups.[1].Value.Chars 0 |> registerFromChar, m.Groups[2].Value |> Int32.Parse)
+        (m.Groups.[1].Value.Chars 0 |> registerFromChar, m.Groups[2].Value |> Int64.Parse)
    
     let parseProgram (line: string) =
         let m = Regex.Match(line, @"Program: (.+)$")
@@ -61,16 +61,16 @@ let setCopy (i: int) (v: 'a) (arr: 'a array) : 'a array =
     cpy[i] <- v
     cpy
 
-let setReg (ps: ProgramState) (reg: Register) (value: int) : ProgramState =
+let setReg (ps: ProgramState) (reg: Register) (value: int64) : ProgramState =
     { ps with regs = setCopy (regToInt reg) value ps.regs }
 
-let setRegAndAdvance (ps: ProgramState) (reg: Register) (value: int) : ProgramState =
+let setRegAndAdvance (ps: ProgramState) (reg: Register) (value: int64) : ProgramState =
     { ps with regs = setCopy (regToInt reg) value ps.regs; ip = ps.ip + 2 }
     
-let getReg (ps: ProgramState) (reg: Register) : int =
+let getReg (ps: ProgramState) (reg: Register) : int64 =
     ps.regs[regToInt reg]
 
-let getComboOp (ps: ProgramState) : int =
+let getComboOp (ps: ProgramState) : int64 =
     let op = ps.ops[ps.ip + 1]
     match op with
     | n when n >= 0 && n <= 3 -> n
@@ -88,7 +88,7 @@ let halt (ps: ProgramState) : ProgramState = { ps with halt = true }
 let runOneOp (ps: ProgramState) : ProgramState =
     let ip = ps.ip
     if ip >= ps.ops.Length || ip < 0 then
-        printfn $"invalid ip: %d{ip}"
+        // printfn $"invalid ip: %d{ip}"
         halt ps
     else
     
@@ -96,7 +96,7 @@ let runOneOp (ps: ProgramState) : ProgramState =
     | 0 (* adv *) ->
         let n = getReg ps A
         let co = getComboOp ps
-        let d = 1 <<< co
+        let d = 1L <<< (int co)
         let r = n / d
         
         // printfn $">%02d{ip}: adv %d{n} %d{d} -> %d{r}"
@@ -114,7 +114,7 @@ let runOneOp (ps: ProgramState) : ProgramState =
         
     | 2 (* bst *) ->
         let n = getComboOp ps
-        let r = n % 8
+        let r = n % 8L
         
         // printfn $">%02d{ip}: bst %d{n} -> %d{r}"
         
@@ -140,7 +140,7 @@ let runOneOp (ps: ProgramState) : ProgramState =
         
     | 5 (* out *) ->
         let n = getComboOp ps
-        let r = n % 8
+        let r = n % 8L |> int
         
         // printfn $">%02d{ip}: out %d{n} -> %d{r}"
         
@@ -149,7 +149,7 @@ let runOneOp (ps: ProgramState) : ProgramState =
     | 6 (* bdv *) ->
         let n = getReg ps A
         let co = getComboOp ps
-        let d = 1 <<< co
+        let d = 1L <<< (int co)
         let r = n / d
         
         // printfn $">%02d{ip}: bdv %d{n} %d{d} -> %d{r}"
@@ -159,7 +159,7 @@ let runOneOp (ps: ProgramState) : ProgramState =
     | 7 (* cdv *) -> 
         let n = getReg ps A
         let co = getComboOp ps
-        let d = 1 <<< co
+        let d = 1L <<< (int co)
         let r = n / d
         
         // printfn $">%02d{ip}: cdv %d{n} %d{d} -> %d{r}"
@@ -215,16 +215,43 @@ module PartTwo =
         Register B: 0
         Register C: 0
 
-        Program: 0,3,5,4,3,0 
+        Program: 0,3,5,4,3,0
         """
-        
-    let solve (ps: ProgramState) : int =
-        let tryAValue (a: int) =
-            let ps = setReg ps A a |> runUntilHalt 
-            let out = ps.out |> List.rev |> List.toArray
-            out = ps.ops
-        [0..10_000_000] |> Seq.find tryAValue
-     
+         
+    let fromOctal (str: string) : int64 = Convert.ToInt64(str, 8)
+    
+    let fromOctalDigits (digits: int seq) : int64 =
+        String.Join("", digits |> Seq.rev |> Seq.toArray) |> fromOctal
+
+    let reset (a: int64) (ps: ProgramState) : ProgramState =
+        { ps with halt = false; ip = 0; out = []; regs = [| a; 0L; 0L |] }
+    
+    let solve (ps: ProgramState) : int64 =
+            
+        let firstDigitOfOutput (a: int64) =
+            ps
+            |> reset a
+            |> runUntilHalt
+            |> (_.out)
+            |> List.last
+            
+        let rec loop (digits: int list) = function
+            | [] -> Some digits
+            | (nextProgramDigit :: programDigits) ->
+            
+                let checkDigit (d: int) =
+                    let res = fromOctalDigits (d :: digits) |> firstDigitOfOutput
+                    res = nextProgramDigit
+                    
+                [0..7]
+                |> Seq.filter checkDigit
+                |> Seq.choose (fun d -> loop (d :: digits) programDigits)
+                |> Seq.tryHead
+            
+        let reversedProgramDigits = ps.ops |> Array.toList |> List.rev
+       
+        loop [] reversedProgramDigits |> Option.get |> fromOctalDigits
+    
     [<Test>] 
     let ``example input``() =
         exampleInput
@@ -237,6 +264,4 @@ module PartTwo =
         readTextFromFile @"Days/Day17/input.txt"
         |> parseProgram
         |> solve
-        |> should equal 0
-
-    
+        |> should equal 109685330781408L
